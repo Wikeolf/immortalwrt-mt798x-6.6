f9c08e9858d32732678419b927ac727233bd267a
Wikeolf <Wikeolf@Gmail.com>
2025-06-29 13:29:37 +0800

    muvirtbuild: Optimized the build process to fix missing default device packages during device selection Added muvirt-tiny configuration Tested compressed-memory package Updated muvirt and compressed packages to custom sources
    Signed-off-by: Wikeolf <Wikeolf@Gmail.com>

--- muvirtbuild.sh
+++ muvirtbuild.sh
@@ -27,20 +27,22 @@ TARGET=${2:-TARGET_mediatek_filogic}
 # add muvirt feeds
 echo "Adding muvirt feeds..."
 cat > feeds.conf <<EOF
-src-git muvirt https://gitlab.com/traversetech/muvirt-feed.git
+# src-git muvirt https://gitlab.com/traversetech/muvirt-feed.git
+# src-git notengobattery https://github.com/NoTengoBattery/openwrt-custom-feed.git
 src-git ten64 https://gitlab.com/traversetech/ls1088firmware/ten64-openwrt-feed.git
 src-git openwisp https://github.com/openwisp/openwisp-config.git
-src-git notengobattery https://github.com/NoTengoBattery/openwrt-custom-feed.git
+src-link wikeolf /tmp/Wikeolf-feeds
 EOF
 
 # update and install feeds and patch qemu deps
 echo "Updating and installing feeds..."
 ./scripts/feeds update -a
-./scripts/feeds install -p muvirt -a 
+# ./scripts/feeds install -p muvirt -a
+./scripts/feeds install -p wikeolf -a 
 ./scripts/feeds install tpm-tools
 ./scripts/feeds install trousers
-./scripts/feeds install luci-app-compressed-memory
-./scripts/feeds install compressed-memory
+# ./scripts/feeds install luci-app-compressed-memory
+# ./scripts/feeds install compressed-memory
 ./scripts/feeds install -p openwisp openwisp-config
 
 echo "Patching qemu dependencies..."
@@ -51,18 +53,17 @@ sed package/feeds/packages/qemu/Makefile -i \
 # apply default configuration
 echo "Applying default configuration..."
 cp defconfig/${CONF} .config
-make defconfig
 
-# check dependencies in .config
-grep "CONFIG_PACKAGE_muvirt=y" .config || (echo "muvirt not selected" && exit 1)
-grep "CONFIG_PACKAGE_luci=y" .config || (echo "LuCI not selected" && exit 1)
-grep "CONFIG_PACKAGE_lvm2=y" .config || (echo "LVM not selected" && exit 1)
-grep "CONFIG_PACKAGE_tmux=y" .config || (echo "tmux not selected" && exit 1)
-grep "CONFIG_PACKAGE_qemu-img" .config || (echo "qemu-img not selected" && exit 1)
-grep "CONFIG_PACKAGE_qemu-nbd" .config || (echo "qemu-nbd not selected" && exit 1)
-grep "CONFIG_PACKAGE_ethtool-full=y" .config || (echo "ethtool-full not selected" && exit 1)
-grep "CONFIG_PACKAGE_luci-proto-wireguard=y" .config || (echo "luci-proto-wireguard not selected" && exit 1)
-grep "CONFIG_PACKAGE_qrencode=y" .config || (echo "qrencode not selected" && exit 1)
+# # check dependencies in .config
+# grep "CONFIG_PACKAGE_muvirt=y" .config || (echo "muvirt not selected" && exit 1)
+# grep "CONFIG_PACKAGE_luci=y" .config || (echo "LuCI not selected" && exit 1)
+# grep "CONFIG_PACKAGE_lvm2=y" .config || (echo "LVM not selected" && exit 1)
+# grep "CONFIG_PACKAGE_tmux=y" .config || (echo "tmux not selected" && exit 1)
+# grep "CONFIG_PACKAGE_qemu-img" .config || (echo "qemu-img not selected" && exit 1)
+# grep "CONFIG_PACKAGE_qemu-nbd" .config || (echo "qemu-nbd not selected" && exit 1)
+# grep "CONFIG_PACKAGE_ethtool-full=y" .config || (echo "ethtool-full not selected" && exit 1)
+# grep "CONFIG_PACKAGE_luci-proto-wireguard=y" .config || (echo "luci-proto-wireguard not selected" && exit 1)
+# grep "CONFIG_PACKAGE_qrencode=y" .config || (echo "qrencode not selected" && exit 1)
 
 echo "Now you can select additional packages with 'make menuconfig'"
 echo "and then build with 'make -j$(nproc)'"
