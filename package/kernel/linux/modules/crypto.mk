d9798226ec8b8d71570db48d2faada27b5b10ff8
Wikeolf <Wikeolf@Gmail.com>
2025-07-30 09:38:51 +0800

    custom: remove crypto-zstd
    Signed-off-by: Wikeolf <Wikeolf@Gmail.com>

--- package/kernel/linux/modules/crypto.mk
+++ package/kernel/linux/modules/crypto.mk
@@ -1204,18 +1204,3 @@ define KernelPackage/crypto-xxhash
 endef
 
 $(eval $(call KernelPackage,crypto-xxhash))
-
-define KernelPackage/crypto-zstd
-  TITLE:=ZSTD compression CryptoAPI module
-  DEPENDS:=+kmod-lib-zstd +kmod-crypto-acompress
-  KCONFIG:=CONFIG_CRYPTO_ZSTD
-  FILES:=$(LINUX_DIR)/crypto/zstd.ko
-  AUTOLOAD:=$(call AutoLoad,09,zstd)
-  $(call AddDepends/crypto)
-endef
-
-define KernelPackage/crypto-zstd/description
-  Zstandard is a fast lossless compression algorithm, targeting real-time
-endef
-
-$(eval $(call KernelPackage,crypto-zstd))
